0b15523645147d5b109b9dda44c0a34db8d658fc
Pawel Konior <pawel.konior@gmail.com>
2023-09-23 12:05:47 +0200

    add offer to cart

--- src/components/offers/OfferDetails.jsx
+++ src/components/offers/OfferDetails.jsx
@@ -1,10 +1,12 @@
 import {Link, useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import axios from "axios";
+import CartContext from "../../contexts/CartContext.jsx";
 
 function OfferDetails() {
     const {offerId} = useParams();
     const [offer, setOffer] = useState(null);
+    const {cart, setCart} = useContext(CartContext);
 
     useEffect(() => {
         getOffer(offerId).then(({data}) => setOffer(data));
@@ -18,12 +20,23 @@ function OfferDetails() {
         return <div>Loading...</div>
     }
 
+    function addToCart() {
+        setCart([...cart, {
+            title: offer.title,
+            price: offer.price,
+            id: offer.id
+        }])
+    }
+
     return (
         <div>
             <h1>{offer.title}</h1>
             <p>{offer.description}</p>
             <p>Cena: {offer.price} PLN</p>
-            <button>Kup</button>
+            <button
+                disabled={cart.some((cartOffer) => cartOffer.id === offer.id)}
+                onClick={addToCart}
+            >Kup</button>
             <Link to={'/'}>Powrót do wyszukiwania</Link>
         </div>
     );
