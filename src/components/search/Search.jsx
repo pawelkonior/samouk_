823ae69331086a55b7249c00548dcb78e454b189
Pawel Konior <pawel.konior@gmail.com>
2023-09-23 09:25:54 +0200

    search component with proxy vite configuration

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,6 +1,26 @@
+import {useState} from "react";
+import axios from "axios";
+
 function Search() {
+    const [offer, setOffer] = useState('');
+
+
+    async function getOffers() {
+        return axios.get('/offers');
+    }
+
     return (
-        <div>Search</div>
+        <div>
+            <input
+                type="text"
+                placeholder="Szukaj ofert"
+                value={offer}
+                onChange={(event) => setOffer(event.target.value)}
+            />
+            <section>
+                results
+            </section>
+        </div>
     );
 }
 
